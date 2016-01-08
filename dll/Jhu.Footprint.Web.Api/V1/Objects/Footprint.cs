01a3c16304c026f5e1a5a888cc05376147d7caf2
Laszlo Dobos <dobos@complex.elte.hu>
2016-01-08 16:11:14 +0100

    REST web API skeleton added.

---
+++ dll/Jhu.Footprint.Web.Api/V1/Objects/Footprint.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+using System.ComponentModel;
+
+namespace Jhu.Footprint.Web.Api.V1
+{
+    [DataContract(Name = "footprint")]
+    [Description("TODO")]
+    public class Footprint
+    {
+        [DataMember(Name = "name")]
+        [Description("Name of the footprint.")]
+        public string Name { get; set; }
+
+        public Footprint()
+        {
+        }
+    }
+}
