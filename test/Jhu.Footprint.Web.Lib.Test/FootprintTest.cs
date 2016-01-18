3b2981717a71f1b1315397ed32e7fda8c24abb78
Evelin Banyai <evelin.banyai@gmail.com>
2016-01-18 14:52:46 +0100

    Added FootprintTest.cs

---
+++ test/Jhu.Footprint.Web.Lib.Test/FootprintTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jhu.Footprint.Web.Lib.Test
+{
+    [TestClass]
+    public class FootprintTest
+    {
+        [TestMethod]
+        public void CreateTest()
+        {
+            using (var context = new Context())
+            {
+                var footprint = new Footprint(context);
+
+                footprint.Name = "Csík";
+                footprint.User = "webtestuser";
+                footprint.Public = 1;
+                footprint.FillFactor = 0.9;
+                footprint.FolderId = 2;
+                footprint.FolderType = FolderType.None;
+
+                footprint.Create();
+            }
+        }
+    }
+}
