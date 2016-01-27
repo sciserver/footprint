bfefa751e63a85d701fb23c256108cbfefd66ded
Evelin Banyai <evelin.banyai@gmail.com>
2016-01-27 15:18:43 +0100

    Added test project for the Api part.

---
+++ test/Jhu.Footprint.Web.Api.Test/FootprintTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Mime;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jhu.Footprint.Web.Api.Test
+{
+    [TestClass]
+    public class FootprintTest
+    {
+        [TestMethod]
+        public void GetUserFootprintTest()
+        {
+            var uri = new Uri("http://localhost/footprint/api/v1/Footprint.svc/users/evelin/footprints/SDSS.DR7/Stripe2");
+            var req = (HttpWebRequest)WebRequest.Create(uri);
+
+            req.Accept = MediaTypeNames.Text.Xml;
+
+            var res = (HttpWebResponse)req.GetResponse();
+
+            using (var reader = new StreamReader(res.GetResponseStream()))
+            {
+                var text = reader.ReadToEnd();
+                System.Diagnostics.Debug.WriteLine(text);
+            }
+        }
+    }
+}
