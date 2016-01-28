532d2378d050a34972d6bb3ce626a1ef8e21ddb7
Evelin Banyai <evelin.banyai@gmail.com>
2016-01-28 16:11:26 +0100

    Tests run with authentication + added CreateUserFootprintTest().

--- test/Jhu.Footprint.Web.Api.Test/FootprintTest.cs
+++ test/Jhu.Footprint.Web.Api.Test/FootprintTest.cs
@@ -4,16 +4,35 @@ using System.IO;
 using System.Net;
 using System.Net.Mime;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Jhu.Graywulf.Web.Api.V1;
+using Jhu.Graywulf.Web.Services;
 
-namespace Jhu.Footprint.Web.Api.Test
+namespace Jhu.Footprint.Web.Api.V1
 {
     [TestClass]
-    public class FootprintTest
+    public class FootprintTest : ApiTestBase
     {
+        protected IFootprintService CreateClient(RestClientSession session)
+        {
+            AuthenticateTestUser(session);
+
+            var uri = new Uri("http://CASTOR/footprint/api/v1/Footprint.svc");
+
+            var client = session.CreateClient<IFootprintService>(uri, null);
+            return client;
+        }
+
         [TestMethod]
         public void GetUserFootprintTest()
         {
-            var uri = new Uri("http://localhost/footprint/api/v1/Footprint.svc/users/evelin/footprints/SDSS.DR7/Stripe2");
+            using (var session = new RestClientSession())
+            {
+                var client = CreateClient(session);
+                var footprint = client.GetUserFootprint("evelin","SDSS.DR7","Stripe2");
+            }
+
+            /*
+            var uri = new Uri("http://CASTOR/footprint/api/v1/Footprint.svc/users/evelin/footprints/SDSS.DR7/Stripe2");
             var req = (HttpWebRequest)WebRequest.Create(uri);
 
             req.Accept = MediaTypeNames.Text.Xml;
@@ -24,6 +43,28 @@ namespace Jhu.Footprint.Web.Api.Test
             {
                 var text = reader.ReadToEnd();
                 System.Diagnostics.Debug.WriteLine(text);
+            }
+            */
+        }
+
+        [TestMethod]
+        public void CreatUserFootprintTest()
+        {
+            using (var session = new RestClientSession())
+            {
+                var client = CreateClient(session);
+
+                var request = new FootprintRequest();
+                var footprint = new Lib.Footprint();
+                footprint.Comment = "Test Api Create Footprint";
+                footprint.User = "Evelin";
+                footprint.Name = "Test api";
+                footprint.FolderName = "TestFolder";
+
+                request.Footprint = new V1.Footprint(footprint);
+
+                client.CreateUserFootprint("Evelin","Test","Test",request);
+             
             }
         }
     }
