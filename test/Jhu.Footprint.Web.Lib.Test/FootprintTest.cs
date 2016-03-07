a146ce924dad24476284fd7c8d2df4527bb0ed4b
Evelin Banyai <evelin.banyai@gmail.com>
2016-03-07 13:23:22 +0100

    no functional change

--- test/Jhu.Footprint.Web.Lib.Test/FootprintTest.cs
+++ test/Jhu.Footprint.Web.Lib.Test/FootprintTest.cs
@@ -78,13 +78,21 @@ namespace Jhu.Footprint.Web.Lib.Test
             {
                 var footprint = new Footprint(context);
 
-                footprint.Id = 5;
-                context.User = "mike";
+                footprint.Id = 2;
+                context.User = "evelin";
 
                 footprint.Load();
                 var s = @"REGION
 	CONVEX
-	-0.49240387650610379 0.85286853195244328 0.17364817766693033 0.64278760968653936";
+	-0.70710678118654746 0.70710678118654757 0 0
+	0 0 1 0
+	0.14356697510519473 0.53579924538156265 -0.83205029433784372 0
+	0.96592582628906831 0.25881904510252085 5.5511151231257839E-17 0
+	CONVEX
+	0 0 1 0
+	0 1 0 0
+	0.33081724288831171 0.25384499855699239 -0.9089129048018717 0
+	0.70710678118654746 -0.70710678118654757 0 0";
 
                 footprint.Region = Jhu.Spherical.Region.Parse(s);
                 footprint.Region.Simplify();
