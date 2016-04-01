5eb265ac2bf7282d442f82b44962d264be353f03
Evelin Banyai <evelin.banyai@gmail.com>
2016-04-01 10:49:47 +0200

    Added GalacticPoint class.

---
+++ dll/Jhu.Footprint.Web.Lib/GalacticPoint.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jhu.Footprint.Web.Lib
+{
+    public class GalacticPoint
+    {
+
+        public double L
+        { get; set; }
+
+        public double B
+        { get; set; }
+
+        public static implicit operator GalacticPoint(Point point)
+        {
+                 var b = CoordinateTransformations.GetLatitude(point.Ra, point.Dec);
+            return new GalacticPoint()
+             {
+                 B = b ,
+                 L = CoordinateTransformations.GetLongitude(point.Ra, point.Dec, b)
+             };
+        }
+    }
+}
