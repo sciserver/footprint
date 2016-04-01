eb6c226362e9c019f456680c79d722ed53f57085
Evelin Banyai <evelin.banyai@gmail.com>
2016-04-01 12:22:15 +0200

    Renamed Point to EquatorialPoint, added new implicit operator.

---
+++ dll/Jhu.Footprint.Web.Lib/EquatorialPoint.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Jhu.Spherical;
+
+namespace Jhu.Footprint.Web.Lib
+{
+    public class EquatorialPoint
+    {
+        public double RA { get; set; }
+        public double Dec { get; set; }
+
+        public static implicit operator EquatorialPoint(Cartesian c)
+        {
+            return new EquatorialPoint()
+            {
+                RA = c.RA,
+                Dec = c.Dec
+            };
+        }
+
+        public static implicit operator EquatorialPoint(GalacticPoint gp)
+        {
+            double d = CoordinateTransformations.GetDeclinationFromGalactic(gp.L, gp.B);
+
+            return new EquatorialPoint()
+            {
+                Dec = d,
+                RA = CoordinateTransformations.GetRightAscensionFromGalactic(gp.L, gp.B, d)
+            };
+        }
+    }
+}
