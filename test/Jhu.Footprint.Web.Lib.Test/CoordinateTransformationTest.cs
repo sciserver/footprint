625d870ef34042ea9fbbd19486f3b55482123aa2
Evelin Banyai <evelin.banyai@gmail.com>
2016-04-01 10:44:48 +0200

    Added new Test class for testing coordinate system transformations.

---
+++ test/Jhu.Footprint.Web.Lib.Test/CoordinateTransformationTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Jhu.Spherical;
+
+namespace Jhu.Footprint.Web.Lib.Test
+{
+    [TestClass]
+    public class CoordinateTransformationTest
+    {
+        [TestMethod]
+        public void ConvertToGalacticCoordinatesTest()
+        {
+            Point point = new Cartesian(15, 15);
+            GalacticPoint galPoint = point;
+
+            var output = String.Format("{0}, {1}", galPoint.L, galPoint.B);
+            System.Diagnostics.Debug.WriteLine(output);
+        }
+    }
+}
