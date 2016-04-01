08d3d3df5664400d5ac450bc9afeb0cc8a6023f8
Evelin Banyai <evelin.banyai@gmail.com>
2016-04-01 12:28:27 +0200

    Added new unit test cases.

--- test/Jhu.Footprint.Web.Lib.Test/CoordinateTransformationTest.cs
+++ test/Jhu.Footprint.Web.Lib.Test/CoordinateTransformationTest.cs
@@ -8,13 +8,33 @@ namespace Jhu.Footprint.Web.Lib.Test
     public class CoordinateTransformationTest
     {
         [TestMethod]
-        public void ConvertToGalacticCoordinatesTest()
+        public void ConvertEquatorialToGalacticCTest1()
         {
-            Point point = new Cartesian(15, 15);
+            EquatorialPoint point = new Cartesian(15, 15);
             GalacticPoint galPoint = point;
 
             var output = String.Format("{0}, {1}", galPoint.L, galPoint.B);
             System.Diagnostics.Debug.WriteLine(output);
+        }
+
+        [TestMethod]
+        public void ConvertEquatorialToGalacticTest2()
+        {
+            GalacticPoint galPoint = new Cartesian(15, 15);
+
+            var output = String.Format("{0}, {1}", galPoint.L, galPoint.B);
+            System.Diagnostics.Debug.WriteLine(output);
+        }
+
+        [TestMethod]
+        public void ConvertGalacticToEquatorialTest1()
+        {
+            GalacticPoint gp = new Cartesian(15, 15);
+            EquatorialPoint ep = gp;
+
+            var output = String.Format("{0}, {1}", ep.RA, ep.Dec);
+            System.Diagnostics.Debug.WriteLine(output);
+             
         }
     }
 }
