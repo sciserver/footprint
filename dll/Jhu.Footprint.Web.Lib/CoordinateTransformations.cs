c910d202833487fd7f5b5230a04c40f243f14c17
Evelin Banyai <evelin.banyai@gmail.com>
2016-04-01 12:25:51 +0200

    Added inverse transformation. Tidied up code.

--- dll/Jhu.Footprint.Web.Lib/CoordinateTransformations.cs
+++ dll/Jhu.Footprint.Web.Lib/CoordinateTransformations.cs
@@ -7,17 +7,9 @@ namespace Jhu.Footprint.Web.Lib
 {
     public static class CoordinateTransformations
     {
-        /*
-        public Point GalacticFromEquatorial(Point point)
-        {
-            Point transformed = new Point();
 
-            
-
-            return transformed;
-        }*/
-
-        public static double GetLatitude(double ra, double dec)
+        #region Equatorial to Galactic
+        public static double GetLatitudeFromEquatorial(double ra, double dec)
         {
             ra = ToRad(ra);
             dec = ToRad(dec);
@@ -26,7 +18,7 @@ namespace Jhu.Footprint.Web.Lib
             return ToDeg(b);
         }
 
-        public static double GetLongitude(double ra, double dec, double b)
+        public static double GetLongitudeFromEquatorial(double ra, double dec, double b)
         {
             ra = ToRad(ra);
             dec = ToRad(dec);
@@ -57,7 +49,54 @@ namespace Jhu.Footprint.Web.Lib
 
             return ToDeg(l);
         }
+        #endregion
 
+        #region Galactic to Equatorial
+        public static double GetDeclinationFromGalactic(double l, double b)
+        {
+            l = ToRad(l);
+            b = ToRad(b);
+
+            double sind = Math.Sin(Constants.decGP) * Math.Sin(b) + Math.Cos(Constants.decGP) * Math.Cos(b) * Math.Cos(Constants.lCP - l);
+            double dec = Math.Asin(sind);
+            return ToDeg(dec);
+        }
+
+        public static double GetRightAscensionFromGalactic(double l, double b, double dec)
+        {
+            l = ToRad(l);
+            b = ToRad(b);
+            dec = ToRad(dec);
+
+            // the two equation to get sin(ra - ra_GP) = sinE and cos(ra - ra_GP) = cosE
+            double sinE = (Math.Cos(b) * Math.Sin(Constants.lCP - l)) / Math.Cos(dec);
+            double cosE = (Math.Cos(Constants.decGP) * Math.Sin(b) - Math.Sin(Constants.decGP) * Math.Cos(b) * Math.Cos(Constants.lCP - l)) / Math.Cos(dec);
+
+            double E = Math.Asin(sinE);
+
+            // find the correct quadrant
+            if (sinE >= 0 & cosE < 0 | sinE < 0 & cosE < 0)
+            {
+                E = Math.PI - E;
+            }
+            else if (sinE < 0 & cosE >= 0)
+            {
+                E = Math.PI * 2.0 + E;
+            }
+
+            double ra = Constants.raGP + E;
+
+            // Ensure ra lies in the range 0-2pi 
+
+            if (ra < 0) l = l + (2.0 * Math.PI);
+            if (ra > (2.0 * Math.PI)) l = l - (2.0 * Math.PI);
+
+            return ToDeg(ra);
+        }
+
+        #endregion
+
+        #region Radian - degree conversions
         private static double ToRad(double x)
         {
             return x * 2 * Math.PI / 360;
@@ -67,6 +106,6 @@ namespace Jhu.Footprint.Web.Lib
         {
             return x * 360 / (2 * Math.PI);
         }
-
+        #endregion
     }
 }
