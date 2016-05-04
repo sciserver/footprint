91c5d1ede5357dec755dab39d75e3a2566f0cef3
Evelin Banyai <evelin.banyai@gmail.com>
2016-05-04 15:26:59 +0200

    Frame of editor page (canvas, buttons + 3 modals) + javascript.
    Conflicts:
    	web/Jhu.Footprint.Web.UI/Editor.aspx
    	web/Jhu.Footprint.Web.UI/Editor.aspx.cs
    	web/Jhu.Footprint.Web.UI/Editor.aspx.designer.cs
    	web/Jhu.Footprint.Web.UI/Jhu.Footprint.Web.UI.csproj

---
+++ web/Jhu.Footprint.Web.UI/Scripts/Editor.js
@@ -0,0 +1,108 @@
+﻿
+$(document).ready(function () {
+
+    // TODO: userName +/ baseUrl declaration in a more proper way
+    var userName = "test";
+    var baseUrl = "http://localhost/footprint/api/v1/Footprint.svc/users";
+    var requestedUrl = "";
+
+    // --> Load Modal
+    $("body").on("click", "#LaunchLoadModalButton", function () {
+        $("#LoadModal").modal({ backdrop: "static" });
+        LoadFootprintFolderList(baseUrl + "/" + userName);
+    });
+
+    $("body").on("change", "#FolderSelect", function () {
+        ResetSelectList("#FootprintSelect");
+        var folderName = $("#FolderSelect option:selected").html();
+        LoadFootprintList(baseUrl + "/" + userName + "/" + folderName);
+    });
+
+    $("body").on("hidden.bs.modal", "#LoadModal", function () {
+        ResetSelectList("#FolderSelect");
+        ResetSelectList("#FootprintSelect");
+    });
+
+    $("body").on("click", "#LoadFootprintButton", function () {
+        var footprint = $("#FootprintSelect option:selected").text();
+        var folder = $("#FolderSelect option:selected").text();
+        plotUrl = createUrl(baseUrl, [userName, folder, footprint,"plot?"]);
+
+        $("#PlotCanvas").attr("src", plotUrl);
+        $("#LoadModal").modal("hide");
+    });
+    // <--  Load Modal
+
+    // --> Grow Modal
+    $("body").on("click", "#LaunchGrowModalButton", function () {
+        $("#GrowModal").modal({ backdrop: "static" });
+    });
+
+    // <-- Grow Modal
+
+    // --> Add Region Modal
+    $("body").on("click", "#LaunchAddRegionModalButton", function () {
+        $("#AddRegionModal").modal({ backdrop: "static" });
+    });
+
+    // <-- Add Region Modal
+
+    // Centering modals
+    $("body").on('show.bs.modal', ".modal", function () {
+        centerModals($(this));
+    });
+    $(window).on('resize', centerModals);
+});
+
+// Reset Select options
+function ResetSelectList(selectId) {
+    $(selectId).empty()
+        .append("<option>Please select...</option>");
+    $(selectId + " option:selected").attr('disabled', 'disabled');
+   
+}
+
+// GET Footprint folder list of User
+function LoadFootprintFolderList(serviceUrl) {
+
+    $.get(serviceUrl, function (data, status) {
+        $(data).find("footprintfolder").each(function () {
+            $("#FolderSelect").append($('<option>').append($(this).find("name").text()));
+        })
+    });
+}
+
+// GET selected folder and the footprints within
+function LoadFootprintList(serviceUrl) {
+    $.get(serviceUrl, function (data, status) {
+        var nameList = $(data).find("footprintNameList");
+        nameList.find("string").each(function () {
+            $("#FootprintSelect").append($("<option>").append($(this).text()));
+        })
+    });
+}
+
+// Centering modals
+function centerModals($element) {
+    var $modals;
+    if ($element.length) {
+        $modals = $element;
+    } else {
+        $modals = $(".modal" + ':visible');
+    }
+    $modals.each(function () {
+        var $clone = $(this).clone().css('display', 'block').appendTo('body');
+        var top = Math.round(($clone.height() - $clone.find('.modal-content').height()) / 2);
+        top = top > 0 ? top : 0;
+        $clone.remove();
+        $(this).find('.modal-content').css("margin-top", top);
+    });
+}
+
+function createUrl(baseUrl, sourcePathParts) {
+    var finalUrl = baseUrl;
+    $.each(sourcePathParts, function(i,part){
+        finalUrl += "/" + part;
+    });
+    return finalUrl;
+}
