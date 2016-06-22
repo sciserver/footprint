c1846fe4417023d0c9b4fbf4bbd99102e9606415
ebanyai <evelin.banyai@gmail.com>
2016-06-22 14:50:14 +0200

    started writing unit tests for EditorService (not working)

---
+++ test/Jhu.Footprint.Web.Api.Test/EditorApiTestBase.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Jhu.Graywulf.Web.Api.V1;
+using Jhu.Graywulf.Web.Services;
+using Jhu.Footprint.Web.Lib;
+
+namespace Jhu.Footprint.Web.Api.V1
+{
+    public class EditorApiTestBase : ApiTestBase
+    {
+        protected Spherical.Region Region = Spherical.Region.Parse("CIRCLE J2000 10 10 10");
+        protected Spherical.Region RegionShifted = Spherical.Region.Parse("CIRCLE J2000 9.8 9.8 10");
+
+
+        /*
+         * Test prerequisites in Graywulf Registry:
+         * Users: test, test-admin, test-writer, test-reader
+         * Groups: testgroup
+         * Roles: footprint-admin, footprint-writer, footprint-reader
+         * test-* users in testgroup with role footprint-*
+         * */
+
+        protected const string TestUser = "test";
+        protected const string OtherUser = "other";
+        protected const string TestGroup = "testgroup";
+        protected const string GroupAdminUser = "test-admin";
+        protected const string GroupWriterUser = "test-writer";
+        protected const string GroupReaderUser = "test-reader";
+
+        protected static void InitializeDatabase()
+        {
+            FootprintTestBase.InitializeDatabase();
+        }
+
+        protected Context CreateContext()
+        {
+            return FootprintTestBase.CreateContext();
+        }
+
+        protected IEditorService CreateClient(RestClientSession session)
+        {
+            return CreateClient(session, null);
+        }
+
+        protected IEditorService CreateClient(RestClientSession session, string user)
+        {
+            if (user != null)
+            {
+                AuthenticateUser(session, user);
+            }
+
+            var host = Environment.MachineName;
+            var uri = new Uri("http://" + host + "/footprint/api/v1/Editor.svc");
+            var client = session.CreateClient<IEditorService>(uri, null);
+
+            return client;
+        }
+
+        protected RestClientSession ResetTest(string user)
+        {
+            using (var session = new RestClientSession())
+            {
+                var client = CreateClient(session, user);
+                client.Reset();
+                return session;
+            }
+        }
+
+        protected void NewTest(RestClientSession session, string user)
+        {
+            var client = CreateClient(session, user);
+            client.New(Region);
+        }
+
+        protected Spherical.Region GetTestShape(RestClientSession session, string user)
+        {
+            var client = CreateClient(session, TestUser);
+            return client.GetShape();
+
+        }
+
+
+    }
+}
---
+++ test/Jhu.Footprint.Web.Api.Test/EditorServiceTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.ServiceModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Jhu.Graywulf.Web.Services;
+
+namespace Jhu.Footprint.Web.Api.V1
+{
+    [TestClass]
+    public class EditorServiceTest: EditorApiTestBase
+    {
+        [TestMethod]
+        public void UnionTest()
+        {
+            var session = ResetTest(TestUser);
+            NewTest(session, TestUser);
+            var shape = GetTestShape(session, TestUser);
+            
+        }
+
+        protected void IntersectTest(Spherical.Region region)
+        {
+
+        }
+
+        protected void SubtractTest(Spherical.Region region)
+        {
+
+        }
+
+        protected void GrowTest(double arcmin)
+        {
+
+        }
+
+        protected void LoadTest(string owner, string name, string regionName)
+        {
+
+        }
+
+        protected void SaveTest(string owner, string name, string regionName)
+        {
+
+        }
+
+        protected byte[] GetTestOutline()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected byte[] PlotTestFootprintRegion()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected byte[] PlotTestFootprintRegionAdvanced()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
