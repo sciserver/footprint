edb638f180369ac515c2907c67cfab2250ef2233
ebanyai <evelin.banyai@gmail.com>
2016-07-11 15:06:05 +0200

    added new url navigation

--- web/Jhu.Footprint.Web.UI/Controls/Menu.ascx.cs
+++ web/Jhu.Footprint.Web.UI/Controls/Menu.ascx.cs
@@ -9,6 +9,7 @@ namespace Jhu.Footprint.Web.UI.Controls
             Home.NavigateUrl = Jhu.Footprint.Web.UI.Default.GetUrl();
             Search.NavigateUrl = Jhu.Footprint.Web.UI.Search.GetUrl();
             Edit.NavigateUrl = Jhu.Footprint.Web.UI.Editor.GetUrl();
+            Plot.NavigateUrl = Jhu.Footprint.Web.UI.Plot.GetUrl();
             Docs.NavigateUrl = Jhu.Footprint.Web.UI.Docs.Default.GetUrl();
         }
     }
