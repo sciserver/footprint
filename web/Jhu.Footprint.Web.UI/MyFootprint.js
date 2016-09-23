d2ad15fe6c376aa6cc1af7248a339ffe96da4253
ebanyai <evelin.banyai@gmail.com>
2016-09-23 15:49:49 +0200

    implemented footprint delete (v1)

--- web/Jhu.Footprint.Web.UI/MyFootprint.js
+++ web/Jhu.Footprint.Web.UI/MyFootprint.js
@@ -1,10 +1,26 @@
-﻿$(document).ready(function () {
-    
+﻿var footprintSvcUrl = "http://localhost/footprint/api/v1/footprint.svc";
+
+$(document).one('ready', function () {
+    // Set owner
+    // TODO: include group
+    setOwner();
+});
+
+$(document).ready(function () {
+
+    var user = localStorage.getItem("owner");
+    $(document).on("click", "#DeleteFootprints", function () {
+        var $footprints = $("input[name='deleteCheckbox']:checked");
+        $footprints.each(function () {
+            var footprint = $(this).val()
+            deleteFootprint(user, footprint);
+        });
+    })
 });
 
 // delete region
-function deleteRegion()
-{
-
+function deleteFootprint(user, footprint) {
+    var methodUrl = createUrl(footprintSvcUrl, ["users", user, "footprints", footprint]);
+    serviceCall(methodUrl, "DELETE");
 }
 // edit region
