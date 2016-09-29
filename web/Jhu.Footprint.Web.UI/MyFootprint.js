a91dd74206c7e28e3bac25a5703553441252871a
ebanyai <evelin.banyai@gmail.com>
2016-09-29 14:24:17 +0200

    added switch to deleteFootprint mode

--- web/Jhu.Footprint.Web.UI/MyFootprint.js
+++ web/Jhu.Footprint.Web.UI/MyFootprint.js
@@ -9,18 +9,27 @@ $(document).one('ready', function () {
 $(document).ready(function () {
 
     var user = localStorage.getItem("owner");
+    // delete footprints
+    $(document).on("click", "#SelectFootprints", function () {
+        $(this).addClass("hidden");
+        $(".DeleteFootprints").removeClass("hidden");
+    });
+
+
     $(document).on("click", "#DeleteFootprints", function () {
         var $footprints = $("input[name='deleteCheckbox']:checked");
         $footprints.each(function () {
-            var footprint = $(this).val()
+            var footprint = $(this).val();
             deleteFootprint(user, footprint);
         });
-    })
+
+        $(".DeleteFootprints").addClass("hidden");
+        $("#SelectFootprints").removeClass("hidden");
+    });
 });
 
-// delete region
+// delete footprints
 function deleteFootprint(user, footprint) {
     var methodUrl = createUrl(footprintSvcUrl, ["users", user, "footprints", footprint]);
     serviceCall(methodUrl, "DELETE");
 }
-// edit region
